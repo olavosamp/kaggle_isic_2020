cf341a2749cd66b785177998266fb1efe9da5729
Olavo <olavosamp@poli.ufrj.br>
2020-08-16 01:05:59 -0300

    Add script to downsample dataset images
    Downsamples all train images to width 512. Goal is to reduce train time

---
+++ dataset_stats/convert_dataset_images.py
@@ -0,0 +1,16 @@
+import os
+from glob import glob
+from tqdm import tqdm
+from pathlib import Path
+# from ..lib import dirs
+
+source_dir = "/home/common/datasets/SIIM-ISIC_2020_Melanoma/jpeg/train/"
+dest_dir   = "/home/common/datasets/SIIM-ISIC_2020_Melanoma/jpeg/train_compact/"
+
+# dirs.create_folder(dest_dir)
+
+file_list = glob(source_dir+"*.jpg")
+print("Converting image files...")
+for file_path in tqdm(file_list):
+    file_name = Path(file_path).name
+    os.system("convert \""+file_path+"[512x]\" -set filename:base \"%[basename]\" \""+dest_dir+"/%[filename:base].jpg\"")
